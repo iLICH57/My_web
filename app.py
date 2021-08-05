60700fd4723be16348648ea6c51d015df66c3424
iLICH57 <zvmdv1@mail.ru>
2021-08-05 23:45:47 +0900

    modificate project

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template, request
+from hashlib import sha256
 
 # создание экземпляра сервера
 app = Flask(__name__)
 
 # проверка переменной __name__
 # print(__name__)
+
+def hashing(password, num_char):
+    # кодирование
+    byte_str = password.encode()
+    # хешироапние
+    hash_str = sha256(byte_str)
+    # преобразование в hex-строка
+    if num_char == '-':
+        hex_str = hash_str.hexdigest()
+    else: 
+        hex_str = hash_str.hexdigest()[:int(num_char)] 
+    # возврат строки
+    return hex_str
 
 # основная логика нашего сервера
 @app.route("/")
@@ -22,10 +36,13 @@ def product_page():
         salt = request.form.get("salt")
         num_char = request.form.get("num")
 
-        msg = pwd + salt + num_char
+        msg = hashing(pwd + salt, num_char)
 
     return render_template("product.html", message=msg) 
 
+@app.route("/contact")
+def contact_page():
+    return "<h1>Contact</h1>"
 
 # точка входа
 if __name__ == "__main__":
